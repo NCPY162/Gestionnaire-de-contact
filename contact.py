a69a058669fea13929fae762fe14fa7e57652add
NCPY162 <ymj2xcs6@vde-formation.com>
2024-07-24 10:42:55 +0200

    version 2 du gestionnaire de contact

--- contact.py
+++ contact.py
@@ -1,8 +1,5 @@
-"""GESTION DE CONTACT"""
-
-
 import json
-
+import os
 
 class Contact:
     def __init__(self, id, nom, prenom, telephone, email, adresse, codePostal, ville, pays) -> None:
@@ -23,6 +20,7 @@ class GestionnaireDeContact:
     def __init__(self) -> None:
         self.contacts = []
         self.id_count = 0
+        self.loadContacts()
         self.initialisation()
              
     def initialisation(self) : 
@@ -50,25 +48,26 @@ class GestionnaireDeContact:
 
                 elif(value==0):
                     print("Merci et à la prochaine ! ")
+                    break
 
                 else:
-                    print("Choix invalide ! veuillez reessayer")
+                    print("Choix invalide ! veuillez réessayer")
 
             except ValueError:
-                print("Merci de choisir un numero valide")
+                print("Merci de choisir un numéro valide")
     
     def creerContact(self):
-        self.id = self.id_count + 1
+        self.id_count += 1
         nom = str(input("Entrez votre nom : "))
-        prenom = str(input("Entrez votre prenom : "))
-        telephone = int(input("Entrez votre numero de telephone : "))
+        prenom = str(input("Entrez votre prénom : "))
+        telephone = int(input("Entrez votre numéro de téléphone : "))
         email = str(input("Entrez votre adresse mail : "))
         adresse = str(input("Entrez votre adresse : "))
         codePostal = int(input("Entrez votre code postal : "))
         ville = str(input("Entrez votre ville : "))
         pays = str(input("Entrez votre pays : "))
         
-        contact = Contact(self.id, 
+        contact = Contact(self.id_count, 
                           nom, 
                           prenom, 
                           telephone, 
@@ -77,112 +76,96 @@ class GestionnaireDeContact:
                           codePostal, 
                           ville, 
                           pays)
-        self.contacts.append(contact.__dict__)
+        self.contacts.append(contact)
         self.saveInFile()
         print("Contact créé avec succès!")
     
 
     def saveInFile(self):
-        with open("contact.txt", "w") as file:
-            file.seek(0)
-            for contact in self.contacts:
-                formatted_contact = f"nom: {contact['nom']}\
-                \nprenom: {contact['prenom']}\
-                \ntelephone: {contact['telephone']}\
-                \nemail: {contact['email']}\
-                \nadresse: {contact['adresse']}\
-                \ncode postal: {contact['codePostal']}\
-                \nville: {contact['ville']}\
-                \npays: {contact['pays']}\n\n"
-                
-                file.write(formatted_contact + "\n\n")
+        with open("contacts.json", "w") as file:
+            json.dump([contact.__dict__ for contact in self.contacts], file)
             
-    def readFile(self):
-        file_path = "contact.txt"
-        if(file_path):
-            with open(file_path, "r") as file: 
-                content = file.read().strip().split("\n\n")       
-                for line in content:
-                    contact_json = {}
-                    lines = line.split("\n")
-                    for data in lines : 
-                        if ": " in data:
-                            key, value = data.split(": ", 1)
-                            contact_json[key.strip()] = value.strip()
-                        
-                self.contacts.append(contact_json)
-                print(self.contacts)
+    def loadContacts(self):
+        if os.path.exists("contacts.json"):
+            with open("contacts.json", "r") as file:
+                contacts_data = json.load(file)
+                self.contacts = [Contact(**data) for data in contacts_data]
+                if self.contacts:
+                    self.id_count = max(contact.id for contact in self.contacts)
            
     def afficherContacts(self):
-        try:
+        if self.contacts:
             print("Liste des contacts : ")
-            self.readFile()
-        except:
+            for contact in self.contacts:
+                print(contact)
+        else:
             print("Aucun contact trouvé !")
             
     def modifierContact(self):
-        tel = int(input("Quel est le numero de téléphone du contact à modifier : "))
-        self.readFile()
-        try:
-            for contact in self.contacts:
-                if int(contact['telephone']) == tel:
-                    value = int(input("Que voulez-vous modifier ? \n"
-                          "1- votre nom,\n 2- votre prenom, \n 3- votre telephone, \n"
-                          "4- votre email, \n 5- votre adresse,\n"
-                          "6- votre code postal, \n 7- votre ville,\n"
-                          "8- votre pays,\n 0- pour annuler : "))
+        tel = int(input("Quel est le numéro de téléphone du contact à modifier : "))
+        contact = next((contact for contact in self.contacts if contact.telephone == tel), None)
+        
+        if contact:
+            value = int(input("Que voulez-vous modifier ? \n"
+                              "1- votre nom,\n 2- votre prénom, \n 3- votre téléphone, \n"
+                              "4- votre email, \n 5- votre adresse,\n"
+                              "6- votre code postal, \n 7- votre ville,\n"
+                              "8- votre pays,\n 0- pour annuler : "))
 
-                    if(value == 1):
-                        nouveau_nom = str(input("Saisir le nouveau nom : "))
-                        contact["nom"] = nouveau_nom
+            if value == 1:
+                nouveau_nom = str(input("Saisir le nouveau nom : "))
+                contact.nom = nouveau_nom
 
-                    if(value == 2):
-                        nouveau_prenom = str(input("Saisir le nouveau prenom : "))
-                        contact["prenom"] = nouveau_prenom
+            elif value == 2:
+                nouveau_prenom = str(input("Saisir le nouveau prénom : "))
+                contact.prenom = nouveau_prenom
 
-                    if(value == 3):
-                        nouveau_telephone = str(input("Saisir le nouveau telephone : "))
-                        contact["telephone"] = nouveau_telephone
+            elif value == 3:
+                nouveau_telephone = int(input("Saisir le nouveau téléphone : "))
+                contact.telephone = nouveau_telephone
 
-                    if(value == 4):
-                        nouveau_mail = str(input("Saisir le nouveau mail : "))
-                        contact["email"] = nouveau_mail
-    
-                    if(value == 5):
-                        nouvelle_adresse = str(input("Saisir la nouvelle adresse : "))
-                        contact["adresse"] = nouvelle_adresse
+            elif value == 4:
+                nouveau_mail = str(input("Saisir le nouveau mail : "))
+                contact.email = nouveau_mail
 
-                    if(value == 6):
-                        nouveau_codePostal = str(input("Saisir le nouveau code postal : "))
-                        contact["code postal"] = nouveau_codePostal
+            elif value == 5:
+                nouvelle_adresse = str(input("Saisir la nouvelle adresse : "))
+                contact.adresse = nouvelle_adresse
 
-                    if(value == 7):
-                        nouvelle_ville = str(input("Saisir le nouvelle ville : "))
-                        contact["ville"] = nouvelle_ville
+            elif value == 6:
+                nouveau_codePostal = int(input("Saisir le nouveau code postal : "))
+                contact.codePostal = nouveau_codePostal
 
-                    if(value == 8):
-                        nouveau_pays = str(input("Saisir le nouveau pays : "))
-                        contact["pays"] = nouveau_pays
+            elif value == 7:
+                nouvelle_ville = str(input("Saisir la nouvelle ville : "))
+                contact.ville = nouvelle_ville
 
-                    if (value == 0):
-                        print("Merci et à la prochaine !")
-                else:
-                    print("Le numero saisi n'existe pas!")     
-            # self.saveInFile()
-            print("Modification réussie !")                
-        except:
-            print("Choix invalide! Merci de saisir un numero valide")
+            elif value == 8:
+                nouveau_pays = str(input("Saisir le nouveau pays : "))
+                contact.pays = nouveau_pays
+
+            elif value == 0:
+                print("Merci et à la prochaine !")
+                return
+
+            else:
+                print("Choix invalide! Merci de saisir un numéro valide")
+                return
+                
+            self.saveInFile()
+            print("Modification réussie !")
+        else:
+            print("Le numéro saisi n'existe pas!")     
 
     def supprimerContact(self):
-        value = int(input("Quel est le numéro de téléphone du contact à supprimer ? : "))
-        self.readFile()
-        try:
-            for contact in self.contacts:
-                if(int(contact["telephone"]) == value):
-                    self.contacts.remove(contact)
-                    print("Suppression réussie !", self.contacts)
+        tel = int(input("Quel est le numéro de téléphone du contact à supprimer ? : "))
+        contact = next((contact for contact in self.contacts if contact.telephone == tel), None)
+        
+        if contact:
+            self.contacts.remove(contact)
             self.saveInFile()
-        except:
-            print("Numéro est introuvable!")
+            print("Suppression réussie !")
+        else:
+            print("Numéro introuvable!")
 
 contact = GestionnaireDeContact()
